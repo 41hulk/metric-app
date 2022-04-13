d94ffba38db0f402e5ce395a0b6335003f475ef5
Ntare Guy <gntare2@gmail.com>
2022-04-13 23:11:13 +0200

    Getting data from countries and filter them

---
+++ src/redux/countries/country.js
@@ -0,0 +1,56 @@
+// Actions
+const GET_DATA_COUNTRIES = 'GET_COUNTRIES';
+const ORDER_BY_DEATHS = 'ORDER_BY_DEATHS';
+const ORDER_BY_CASES = 'ORDER_BY_CASES';
+
+// Reducer
+export default function countryReducer(state = [], action) {
+  switch (action.type) {
+    case GET_DATA_COUNTRIES:
+      return action.payload.sort((a, b) => Number(b.totalNewDeaths) - Number(a.totalNewDeaths));
+    case ORDER_BY_DEATHS:
+      return state.slice().sort((a, b) => Number(b.totalNewDeaths) - Number(a.totalNewDeaths));
+    case ORDER_BY_CASES:
+      return state.slice().sort((a, b) => Number(b.totalNewCases) - Number(a.totalNewCases));
+    default: return state;
+  }
+}
+
+// Action creators
+export const orderByDeaths = () => ({
+  type: ORDER_BY_DEATHS,
+});
+
+export const orderByCases = () => ({
+  type: ORDER_BY_CASES,
+});
+
+export const getCountries = () => async (dispatch) => {
+  const getYesterdayDate = (d) => new Date(d.getTime() - 24 * 60 * 60 * 1000);
+  let d = new Date();
+  if (d.getUTCHours() <= 9) {
+    d = getYesterdayDate(d);
+  }
+  const currentDate = d.toLocaleString('en-ZA', { year: 'numeric', month: '2-digit', day: '2-digit' })
+    .replace(/\//g, '-');
+  const url = 'https://api.covid19tracking.narrativa.com/api';
+  const response = await fetch(`${url}/${currentDate}`)
+    .then((res) => (
+      res.json().then((json) => {
+        const { countries } = json.dates[currentDate];
+
+        return Object.keys(countries).map((key, index) => ({
+          id: index,
+          name: countries[key].name,
+          totalNewDeaths: countries[key].today_new_deaths,
+          totalNewCases: countries[key].today_new_confirmed,
+          moreInfoLink: countries[key].links[0].href,
+        }));
+      })
+    ))
+    .catch(() => []);
+  dispatch({
+    type: GET_DATA_COUNTRIES,
+    payload: response,
+  });
+};
